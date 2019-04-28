6c0b4ba6b02265a170692cb7474b84ac33845c61
wangchuanping123 <2055439115@qq.com>
2019-04-28 17:33:28 +0800

    菜品列表页面优化

--- pages/varietyOfDishes/varietyOfDishes.js
+++ pages/varietyOfDishes/varietyOfDishes.js
@@ -6,7 +6,23 @@ Page({
    */
 	data: {
 		check: 0,
-		list: ['', '', '', '']
+		list: ['', '', '', ''],
+		food: [
+			{ id: "1", icon: "/images/gbjd.jpg", name: "宫保鸡丁", details: "红而不辣、辣而不猛、香辣味浓、肉质滑脆。由于其入口鲜辣，鸡肉的鲜嫩配合花生的香脆", price: "99", status: "0", sold: "99" },
+			{ id: "1", icon: "/images/mpdf.jpg", name: "麻婆豆腐", details: "麻来自花椒，辣来自辣椒，这道菜突出了川菜“麻辣”的特点。其口味独特，口感顺滑", price: "89", status: "1", sold: "99" },
+			{ id: "1", icon: "/images/tds.jpg", name: "酸辣土豆丝", details: "色泽光亮，酸辣可口，让人口齿生津，酸辣易下饭", price: "29", status: "0", sold: "99" },
+			{ id: "1", icon: "/images/szt.jpg", name: "红烧狮子头", details: "光闻起来就引动食欲，醇香味浓的肉块与汁液，超级美味", price: "79", status: "1", sold: "99" },
+			{ id: "1", icon: "/images/hgr.jpg", name: "川香回锅肉", details: "制作原料主要有猪肉、青椒、蒜苗等，口味独特，色泽红亮，肥而不腻，入口浓香", price: "59", status: "0", sold: "99" },
+		],
+		food_upper: [
+			{ id: "1", icon: "/images/gbjd.jpg", name: "宫保鸡丁", details: "红而不辣、辣而不猛、香辣味浓、肉质滑脆。由于其入口鲜辣，鸡肉的鲜嫩配合花生的香脆", price: "99", status: "0", sold: "99" },
+			{ id: "1", icon: "/images/tds.jpg", name: "酸辣土豆丝", details: "色泽光亮，酸辣可口，让人口齿生津，酸辣易下饭", price: "29", status: "0", sold: "99" },
+			{ id: "1", icon: "/images/hgr.jpg", name: "川香回锅肉", details: "制作原料主要有猪肉、青椒、蒜苗等，口味独特，色泽红亮，肥而不腻，入口浓香", price: "59", status: "0", sold: "99" },
+		],
+		food_lower: [
+			{ id: "1", icon: "/images/mpdf.jpg", name: "麻婆豆腐", details: "麻来自花椒，辣来自辣椒，这道菜突出了川菜“麻辣”的特点。其口味独特，口感顺滑", price: "89", status: "1", sold: "99" },
+			{ id: "1", icon: "/images/szt.jpg", name: "红烧狮子头", details: "光闻起来就引动食欲，醇香味浓的肉块与汁液，超级美味", price: "79", status: "1", sold: "99" },
+		]
 	},
 
 	selectDispaly: function (e) {
