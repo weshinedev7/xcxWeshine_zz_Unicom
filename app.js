b4084d16cc2c380227a8bb07dde09e3d4447677e
王珊珊 <wangshanshan@wangshanshandeMacBook-Pro.local>
2019-04-30 11:06:49 +0800

    去掉地理授权

--- app.js
+++ app.js
@@ -13,21 +13,7 @@ App({
 
       }
     })
-
-    /**
-     * 获取用户的地理位置
-     * wx.authorize():提前向用户发起授权请求。调用后会立刻弹窗询问用户是否同意授权小程序使用某项功能或获取用户的某些数据，但不会实际调用对应接口。如果用户之前已经同意授权，则不会出现弹窗，直接返回成功
-     */
-    wx.authorize({
-      scope: 'scope.userLocation',
-      success: (res) => {
-        console.log('成功：', res)
-      },
-      fail: (res) => {
-        console.log('失败：', res)
-      },
-    })
-
+    
     // 获取用户信息
     wx.getSetting({
       success: res => {
