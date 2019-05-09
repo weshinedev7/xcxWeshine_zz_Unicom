3bd78ea291aeb1886cdcc8bc114bd1afd063ee6c
王珊珊 <wangshanshan@wangshanshandeMacBook-Pro.local>
2019-05-09 10:06:22 +0800

    分开店家与公司的登录

--- pages/roomLogin/roomLogin.js
+++ pages/roomLogin/roomLogin.js
@@ -45,22 +45,40 @@ Page({
             });
           }
 
-          //成功
           if (res.data.status = '200') {
-            //存用户信息到本地存储
-            wx.setStorageSync('employeeInfo', res.data.info);
+            //员工与预约人登录成功
+            if(res.data.info.role==='2'){
+              //存用户信息到本地存储
+              wx.setStorageSync('employeeInfo', res.data.info);
+              wx.showToast({
+                title: '登录成功',
+                icon: 'succes',
+                duration: 2000,
+                mask: true
+              });
+              setTimeout(function () {
+                wx.switchTab ({
+                  url: '/pages/index/index'
+                })
+              }, 3000)
+            }
 
-            wx.showToast({
-              title: '登录成功',
-              icon: 'succes',
-              duration: 2000,
-              mask: true
-            });
-            setTimeout(function () {
-              wx.switchTab ({
-                 url: '/pages/index/index'
-              })
-            }, 3000)
+            //店家登录成功
+            if(res.data.info.role==='1'){
+              //存用户信息到本地存储
+              wx.setStorageSync('storeInfo', res.data.info);
+              wx.showToast({
+                title: '登录成功',
+                icon: 'succes',
+                duration: 2000,
+                mask: true
+              });
+              setTimeout(function () {
+                wx.redirectTo ({
+                  url: '/pages/varietyOfDishes/varietyOfDishes'
+                })
+              }, 3000)
+            }
           }
         }
       });
@@ -74,6 +92,11 @@ Page({
     if(wx.getStorageSync('employeeInfo')!==''){
       wx.switchTab ({
         url: '/pages/index/index'
+      })
+    }
+    if(wx.getStorageSync('storeInfo')!==''){
+      wx.redirectTo ({
+        url: '/pages/varietyOfDishes/varietyOfDishes'
       })
     }
   },
